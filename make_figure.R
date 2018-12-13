c07d52540844749489e90f20ee39244d0f0705e1
seanjib <seanjib@gmail.com>
2018-12-13 15:52:59 -0700

    fixed axis label for final figure

--- make_figure.R
+++ make_figure.R
@@ -3,26 +3,27 @@
 library(openxlsx)
 library(dplyr)
 library(ggplot2)
+library(tibble)
 
 #Read data in
 data = as.tibble(read.xlsx('journal.pone.0206767.s002.xlsx', colNames=TRUE))
 
 #Give names to the columns, since apparently they don't stay when I import the data
-colnames(data) = c('age', 'income', 'education', 'work', 'ever_bought_lootbox', 'lootbox_spend', 'ever_bought_microtransaction', 'microtransaction_spend',
-                   'pgsi1', 'pgsi2', 'pgsi3', 'pgsi4', 'pgsi5', 'pgsi6', 'pgsi7', 'pgsi8', 'pgsi9')
+#colnames(data) = c('age', 'income', 'education', 'work', 'ever_bought_lootbox', 'lootbox_spend', 'ever_bought_microtransaction', 'microtransaction_spend',
+#                   'pgsi1', 'pgsi2', 'pgsi3', 'pgsi4', 'pgsi5', 'pgsi6', 'pgsi7', 'pgsi8', 'pgsi9')
 
 #Zeroes in the original data are replaced by NA for some reason. This fixes it.
 data[is.na(data)] = 0
 
-#Covert ever_bought_lootbox and ever_bought_microtransaction to boolean values
-data$ever_bought_microtransaction[data$ever_bought_microtransaction == 2] = 0
-data$ever_bought_microtransaction = as.logical(data$ever_bought_microtransaction)
+#Convert ever_bought_lootbox and ever_bought_microtransaction to boolean values
+data$Ever_Bought_Other_Microtransaction[data$Ever_Bought_Other_Microtransaction == 2] = 0
+data$Ever_Bought_Other_Microtransaction = as.logical(data$Ever_Bought_Other_Microtransaction)
 
-data$ever_bought_lootbox[data$ever_bought_lootbox == 2] = 0
-data$ever_bought_lootbox = as.logical(data$ever_bought_lootbox)
+data$Ever_Bought_LootBox[data$Ever_Bought_LootBox == 2] = 0
+data$Ever_Bought_LootBox = as.logical(data$Ever_Bought_LootBox)
 
 #convert pgsi data to usable values
-data = data %>% mutate(pgsi_score = pgsi1+pgsi2+pgsi3+pgsi4+pgsi5+pgsi6+pgsi7+pgsi8+pgsi9)
+data = data %>% mutate(pgsi_score = PGSI1+PGSI2+PGSI3+PGSI4+PGSI5+PGSI6+PGSI7+PGSI8+PGSI9)
 amount_categories = c('Never bought a loot box',
                       'Less than $1',
                       '$1-$5',
@@ -55,9 +56,9 @@ for(i in 1:nrow(data)) {
     severity = 'high-risk gambler'
   }
   data[[i,'pg_severity']] = severity
-  
-  data[[i, 'lootbox_spend_discrete']] = amount_categories[data[[i, 'lootbox_spend']] + 1]
-  data[[i, 'microtransaction_spend_discrete']] = amount_categories[data[[i, 'microtransaction_spend']] + 1]
+  data$OtherMicrotransactionSpend
+  data[[i, 'lootbox_spend_discrete']] = amount_categories[data[[i, 'LootboxSpend']] + 1]
+  data[[i, 'microtransaction_spend_discrete']] = amount_categories[data[[i, 'OtherMicrotransactionSpend']] + 1]
 }
 
 #Factor severity in order for plotting
@@ -70,7 +71,7 @@ data$pg_severity = factor(data$pg_severity,
 
 
 #Plot the data
-ggplot(data = data, aes(x = pg_severity, y = lootbox_spend + 1)) +
+ggplot(data = data, aes(x = pg_severity, y = LootboxSpend + 1)) +
   stat_boxplot(geom = 'errorbar') +
   #This geom_point is necessary to add discrete labels to a boxplot
   geom_point(aes(y = lootbox_spend_discrete), alpha = 0) +
@@ -79,7 +80,7 @@ ggplot(data = data, aes(x = pg_severity, y = lootbox_spend + 1)) +
   geom_boxplot(fill = 'steelblue3', outlier.fill = 'white', outlier.shape = 21) +
   scale_x_discrete(labels=c('Non problem\ngamblers', 'Low-risk\ngamblers', 'Moderate-risk\ngamblers', 'Problem\ngamblers')) +
   xlab('Severity of Problem Gambling') +
-  ylab('Amount spent on other microtransactions per month') +
+  ylab('Amount spent on loot boxes per month') +
   theme_bw() +
   theme(panel.grid.major.x = element_blank()) +
   geom_hline(yintercept = 1, color = 'black', size=0)
